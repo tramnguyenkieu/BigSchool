70374d0ec8ea7b944c7e3f407bf3112f639ed2c7
kieutramnguyen <tramanh0219@gmail.com>
2019-03-28 09:45:26 +0700

    big

---
+++ BigSchool/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.Models
+{
+    public class Course
+    {
+        public int id { get; set; }
+        public ApplicationUser Lecturer { get; set; }
+
+    }
+}
